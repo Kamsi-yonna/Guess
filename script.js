6d99a1c973912167ab14275a8d9f9056a7096a8c
Kamsiyonna <etiobikamsi@gmail.com>
2023-05-05 15:31:19 +0100

    font

--- script.js
+++ script.js
@@ -1,133 +1,147 @@
-'use strict';
+'strict mode'
 
-// console.log(document.querySelector('.message').textContent);
 
-// document.querySelector('.message').textContent = '😱 You got that right!'
-// console.log(document.querySelector('.message').textContent);
+//Identifying elements I would be adjusting 
 
-// document.querySelector('.number').textContent = '5';
-// for (let i = 0; i < 10; i++) {
-//     console.log(document.querySelector('.number').textContent = `${i}`)
-// } //JUST HAVING SOME FUN
+// document.querySelector('.score').textContent = 15;
+// document.querySelector('.message').textContent = 'start typing.';
+// document.querySelector('.number').textContent = '3';
+// document.querySelector('.again').textContent = 'refresh';
 
-// document.querySelector('.number').textContent = '22';
 
-// document.querySelector('.score').textContent = '12';
+//Giving the secret number a random value (I will make it a known value during the tests)
 
-// // console.log(document.querySelector('.guess').value);
+let number = Math.trunc(Math.random() * 20) + 1; //random
+// let number = 10; //fixed for testing
+// console.log(number)
+// --------------------------------------------------------
 
-//We use the value property to get the value of inputs
-// document.querySelector('.guess').value = '23'
-// console.log(document.querySelector('.guess').value);
-// document.querySelector('.check').addEventListener('click', function (){
-//     console.log(document.querySelector(".guess").value)
-
-// })
-
-//For the secret number, we need it to be random. We will create a function
-
-// const number = Math.trunc(Math.random() * 20) + 1;
-let number = 10;
-console.log(number);
-let score = 10;//halfway point for the game
+//We will create an initial value for the score. This will either increase or decrease during the game
+let score = 10;
 document.querySelector('.score').textContent = score;
-// document.querySelector('.number').textContent = number; //And it worked
-let highScore = 0
-
-//I want the bgcolor and font color to change when the user loses the game
-// document.getElementById("again").style.backgroundColor;
-// document.getElementById("again").style.color;
-// document.getElementById("again").textContent;
 
 
-document.querySelector('.check').addEventListener('click', function (){
-    const guess = Number(document.querySelector('.guess').value);
-    // console.log(typeof guess, guess);
+//Now for the highScore. We will store it in a variable
+let highScore = 0; //it should be zero because it will always begin with zero. For the values during the game, the high score only changes when we win
 
-    //first thing to check would be for empty/falsy values. So before anything, There MUST be a value
-    //if guess variable doesnt have a value (!guess)
+//We need to create an event to the check button once it is clicked
+document.querySelector('.check').addEventListener('click', function(){
+    //in this, we will add/create functions for every scenario that will happen when we click the check button
+    
+    //The input box is where the number will be typed in. We need to be able to get that number and compare it to different situations that could/would occure in the game
+    //to get the value of whatever number in the input field
+    let guess = Number(document.querySelector('.guess').value);
+    console.log(typeof guess, guess);
+
+    //The first scenario to check is an empty input box // No answer.
+    //i.e if there is no number or guess is empty
     if(!guess) {
-        document.querySelector('.message').textContent = 'Please type a Number 😟';
-        // alert('Please type in a Number');
-        
-        
-        //if the guess is equal to the secret number
-    } else if (guess === number) {
-        document.querySelector('.message').textContent = 'Great Guess!😀';
-        //Now we want to add the score by a point when the user gets it right
-        score++;
-        document.querySelector('.score').textContent = score;
-        document.querySelector('.number').textContent = number;
-
-        
-
-        //for the high score
-        if(score > highScore){
-            highScore = score;
-            document.querySelector.apply('.highScore').textContent = highScore
-        }
-        
-        
-        //if guess is greater than secret Num
-    } else if (guess > number && score > 0) {
-
-        document.querySelector('.message').textContent = 'Too high 🙄';
-        //Now we want to reduce the score by a point when the user gets it wrong
-        // document.querySelector('.score').textContent = score--; WHY DOESNT THIS WORK?
-        score--;
-        document.querySelector('.score').textContent = score;
-        
-        //if guess is greater than secret Num
-    } else if (guess < number && score > 0) {
-        document.querySelector('.message').textContent = 'Too low 😫';
-        //Now we want to reduce the score by a point when the user gets it wrong
-        score--;
-        document.querySelector('.score').textContent = score;
-        
-
-        //NOT WORKING TOO
-    } else if (score === 0 ){
-        document.querySelector('.message').textContent = 'Try again perhaps 😅';
-        // document.querySelector('.score').textContent = 0;
-        // document.getElementsByClassName('.again').style.backgroundColor = "lightblue";
-        document.getElementById("again").style.backgroundColor = "#bb1818";
-        document.getElementById("again").style.color = "#f1f1f1";
-        document.getElementById("again").textContent = 'TRY AGAIN PERHAPS 😅';
-        document.querySelector('.number').textContent = number;
-
-
+        //insted of cl, we would directly change the texts in the game
+        document.querySelector('.message').textContent = 'Please Type a Number 😟'; //NICE
     } 
-    // else if (score === 20){
-    //     document.querySelector('.message').textContent = 'You are fantastic! 🎉';
-    // }
 
-})
+    //The next scenario would be if there is a win. i.e the guess is equal to the secret number
+    else if(guess === number){
+        document.querySelector('.message').textContent = 'Great Guess!😀'; //NICE
 
-document.querySelector('.again').addEventListener('click', function(){
-    score = 20;
-    number = 11;
+        //we also want the score to increase when the guess is correct. We will create a variable for initial score
+        // score++;
+        // document.querySelector('.score').textContent = score;
+        //apparently we do not need this for this kind of game
+
+        //we also want the brain logo to show the number when it is correct
+        document.querySelector('.number').textContent = number;
+
+        //we will also disable the input field and the check button
+        document.querySelector('.check').disabled = true; 
+        document.querySelector('.guess').disabled = true; 
+
+        //For a fun css, I want some styles to also change
+        document.getElementById("again").style.backgroundColor = "#8ff473";
+        document.getElementById("again").style.color = "#333";
+        document.getElementById("again").textContent = 'Go Again!🤭';
+        document.querySelector('.number').textContent = number;
+
+        //I should set a time out here so the page can return to default
+
+        //for the highscore, when we win the game, it will update to the previous high of the score variable
+        if(score > highScore) {
+            highScore = score;
+            document.querySelector('.highscore').textContent = highScore;
+        }
+    }
+
+    //The next scenario would be if the guess is not equal to the number
+    else if(guess !== number){
+        // document.querySelector('.message').textContent = guess > 20  ? '1 to 20!🙄' : ''; //NICE
+        // document.querySelector('.message').textContent = guess > number / 2 ? 'almost there!😁' : ''; //NICE
+        // document.querySelector('.message').textContent = guess > number ? 'Woah! Too high!😥' : 'Too low!🙄'; //NICE
+        if (guess > 20) {
+            document.querySelector('.message').textContent = '1 to 20!🙄';
+          } else if (guess >= number - 2 && guess <= number + 2) {
+            document.querySelector('.message').textContent = 'almost there!😁';
+          } else if (guess > number + 2) {
+            document.querySelector('.message').textContent = 'Woah! Too high!😥';
+          } else if (guess < number - 2){
+            document.querySelector('.message').textContent = 'Too low!🙄';
+          }
+          //I HAVE NOW SHOWN THAT I AM A GENIUS!!
+        
+
+        //we also want the score to decrease when the guess is wrong. We will create a variable for initial score
+        // score = score - 1; //We can simplify this
+        score--;
         document.querySelector('.score').textContent = score;
-        document.getElementById("again").style.backgroundColor = '#f1f1f1';
-        document.getElementById("again").style.color = '#000';
-        document.getElementById("again").textContent = 'NEW GAME';
-        document.querySelector('.message').textContent = 'Start guessing...' ;
-        document.querySelector('.number').textContent = '🧠' ;
-        document.querySelector('.guess').value = '';
 
-    // if (score = 20){
-    //     document.querySelector('.score').textContent = score;
-    //     document.getElementById("again").style.backgroundColor = '#f1f1f1';
-    //     document.getElementById("again").style.color = '#000';
-    //     document.querySelector('.message').textContent = 'Start guessing...' ;
-    //     document.querySelector('.number').textContent = '🧠' ;
+        //for the deduction, we want a limit. So certain things will happen when the score gets to 0. To mark the end of the game
+        if (score === 0) {
+            document.querySelector('.message').textContent = "Oooh,try Again 🤕";
+            //we will also disable the input field and the check button
+            document.querySelector('.check').disabled = true; 
+            document.querySelector('.guess').disabled = true; 
 
+            //For a fun css, I want some styles to also change
+            document.getElementById("again").style.backgroundColor = "#bb1818";
+            document.getElementById("again").style.color = "#f1f1f1";
+            document.getElementById("again").textContent = 'TRY AGAIN PERHAPS 😅';
+            document.querySelector('.number').textContent = number;
+        }
 
-    // } else {
-    //     document.querySelector('.score').textContent = score;
-    //     document.getElementById("again").style.backgroundColor = '#ccc';
-    //     document.getElementById("again").style.color = '#fff';
-    //     document.querySelector('.message').textContent = 'S2222.' ;
-    //     document.querySelector('.number').textContent = '222' ;
+    }
 
-    // }
 })
+//NOW I AM CLOSE TO BECOMING A GAME MASTER.
+//Next, I want to be able to reload the page when the user clicks the again button  
+
+
+//We start by selecting the button
+document.querySelector('.again').addEventListener('click', function () {
+    score = 10
+    number = Math.trunc(Math.random() * 20) + 1; //random
+    console.log(number)
+
+    //for the edited texts
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.message').textContent = 'Start guessing...';
+    document.querySelector('.guess').value = '';
+    document.querySelector('.number').textContent = '🧠';
+    document.getElementById("again").textContent = 'NEW GAME';
+
+    //For a fun css, I want some styles to also change
+    document.getElementById("again").style.backgroundColor = "#f1f1f1";
+    document.getElementById("again").style.color = "#333";
+
+
+    //we will also disable the input field and the check button
+    document.querySelector('.check').disabled = false; 
+    document.querySelector('.guess').disabled = false; 
+
+
+    //for the edited css
+    // document.getElementById("again").style.backgroundColor = "#f1f1f1";
+    // document.getElementById("again").style.color = "#333";
+
+})
+
+//GENIUS
+//TIME TO GO LIVE
